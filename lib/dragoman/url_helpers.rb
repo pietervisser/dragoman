8e8bef44d3350d7e3e9839951de175535c614c47
Robert Jan de Gelder <robertjan@rjdg.nl>
2015-05-18 21:49:39 +0200

    Removes obsolete locale merge in url helpers

--- lib/dragoman/url_helpers.rb
+++ lib/dragoman/url_helpers.rb
@@ -3,10 +3,10 @@ module Dragoman
 
     def self.add_untranslated_helpers name, path_module, url_module
       path_module.send :define_method, "#{name}_path" do |*args|
-        __send__(Dragoman::UrlHelpers.localized_helper_name(name, :path), *Dragoman::UrlHelpers.args_with_locale(*args))
+        __send__(Dragoman::UrlHelpers.localized_helper_name(name, :path), *args)
       end
       url_module.send :define_method, "#{name}_url" do |*args|
-        __send__(Dragoman::UrlHelpers.localized_helper_name(name, :url), *Dragoman::UrlHelpers.args_with_locale(*args))
+        __send__(Dragoman::UrlHelpers.localized_helper_name(name, :url), *args)
       end
     end
 
@@ -14,15 +14,5 @@ module Dragoman
       locale = I18n.locale
       "#{name}_#{locale}_#{url_helper_type}"
     end
-
-    def self.args_with_locale *args
-      if args[0].is_a?(Hash)
-        args[0].merge!({locale: I18n.locale})
-      else
-        args << {locale: I18n.locale}
-      end
-      args
-    end
-
   end
 end
